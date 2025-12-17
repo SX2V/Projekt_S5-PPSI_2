a815466d6749bae930d075e486076aa3d032ba9e
Sigurvegarinn2000 <139846743+Sigurvegarinn2000@users.noreply.github.com>
2025-12-17 20:18:41 +0100

    Create StravaUser.cs

---
+++ SportConnect.API/Models/StravaUser.cs
@@ -0,0 +1,10 @@
+//new
+
+public class StravaUser
+{
+    public long Id { get; set; }
+    public string Username { get; set; }
+    public string? Email { get; set; }
+    public string Firstname { get; set; }
+    public string Lastname { get; set; }
+}
